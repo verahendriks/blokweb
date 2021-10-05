f85d72ea262380cf17061bbc198a8553d78ccc3c
verahendriks <90202638+verahendriks@users.noreply.github.com>
2021-10-05 16:26:39 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,48 @@
-// JavaScript Document
-var menu = document.querySelector('.mobile-menu');
-var menuButton = document.querySelector('.open-menu');
-var closeMenuButton = document.querySelector('.close-menu');
-menuButton.addEventListener('click', openMenu);
-closeMenuButton.addEventListener('click', closeMenu);
+// // JavaScript Document
 
-function openMenu(event) {
-    menu.classList.remove('hidden');
+var eersteButtonFooter = document.querySelector("footer section:nth-of-type(1) button");
+var tweedeButtonFooter = document.querySelector("footer section:nth-of-type(2) button");
+var derdeButtonFooter = document.querySelector("footer section:nth-of-type(3) button");
+var vierdeButtonFooter = document.querySelector("footer section:nth-of-type(4) button");
+var vijfdeButtonFooter = document.querySelector("footer section:nth-of-type(5) button");
+
+var eersteSectionFooter = document.querySelector("footer section:nth-of-type(1)");
+var tweedeSectionFooter = document.querySelector("footer section:nth-of-type(2)");
+var derdeSectionFooter = document.querySelector("footer section:nth-of-type(3)");
+var vierdeSectionFooter = document.querySelector("footer section:nth-of-type(4)");
+var vijfdeSectionFooter = document.querySelector("footer section:nth-of-type(5)");
+
+eersteButtonFooter.addEventListener("click", toggleFooterEersteMenuFooter);
+tweedeButtonFooter.addEventListener("click", toggleFooterTweedeMenuFooter);
+derdeButtonFooter.addEventListener("click", toggleFooterDerdeMenuFooter);
+vierdeButtonFooter.addEventListener("click", toggleFooterVierdeMenuFooter);
+vijfdeButtonFooter.addEventListener("click", toggleFooterVijfdeMenuFooter);
+
+function toggleFooterEersteMenuFooter() {
+	eersteSection.classList.toggle("menuOpen");
 }
 
-function closeMenu(event) {
-    menu.classList.add('hidden');
+function toggleFooterTweedeMenuFooter() {
+	tweedeSection.classList.toggle("menuOpen");
+}
+
+function toggleFooterDerdeMenuFooter() {
+	derdeSection.classList.toggle("menuOpen");
+}
+
+function toggleFooterVierdeMenuFooter() {
+	vierdeSection.classList.toggle("menuOpen");
+}
+
+function toggleFooterVijfdeMenuFooter() {
+	vijfdeSection.classList.toggle("menuOpen");
+}
+
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
 }
